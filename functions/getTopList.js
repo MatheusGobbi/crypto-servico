e20f340999d7ada87c4ffa2665ae12d32ddaec44
Matheus Gobbi <mgobbi.10@gmail.com>
2020-03-29 01:49:12 -0300

    Alteracao de link e toFixed no preco

--- functions/getTopList.js
+++ functions/getTopList.js
@@ -7,16 +7,17 @@ const axios = require('axios').default;
  */
 const getTopList = async (req, res) => {
 
-    const topList = await axios.get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=BRL')
+    const topList = await axios.get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=100&tsym=BRL')
 
     const dados = []
+    
     for (const item of topList.data.Data) {
         const infoCrypto = {
             id: item.CoinInfo.Id,
             nome: item.CoinInfo.Name,
             nomeCompleto: item.CoinInfo.FullName,
             img: 'https://www.cryptocompare.com' + item.CoinInfo.ImageUrl,
-            preco: item.RAW.BRL.PRICE
+            preco: Number(item.RAW.BRL.PRICE).toFixed(2)
         }
         dados.push(infoCrypto);
     }
